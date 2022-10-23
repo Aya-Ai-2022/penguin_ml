bc622f3aef1e323f161007bf8d81a8c1f41fd886
Aya <ayasabrycs@gmail.com>
2022-10-23 17:04:10 +0200

    edit file to be secret

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -69,6 +69,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 st.title('Penguin Classifier')
 st.write("This app uses 6 inputs to predict the species of penguin using""a model built on the Palmer's Penguin's dataset. Use the form below"" to get started!")
+
+password_guess = st.text_input('What is the Password?')
+if password_guess != 'streamlit_password':
+    st.stop()
+
 penguin_file = st.file_uploader('Upload your own penguin data')
 # This first section imports the libraries that we need, adds the title – as we have
 # used before, and adds the file_uploader() function. What happens, however,
