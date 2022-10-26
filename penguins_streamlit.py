54afba923ca50a7b253d93611164804071883f14
Caffeine <45335777+Aya-Ai-2022@users.noreply.github.com>
2022-10-26 02:14:45 +0200

    Update penguins_streamlit.py
    without password

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -70,9 +70,9 @@ from sklearn.model_selection import train_test_split
 st.title('Penguin Classifier')
 st.write("This app uses 6 inputs to predict the species of penguin using""a model built on the Palmer's Penguin's dataset. Use the form below"" to get started!")
 
-password_guess = st.text_input('What is the Password?')
-if password_guess != st.secrets["password"]:
-    st.stop()
+# password_guess = st.text_input('What is the Password?')
+# if password_guess != st.secrets["password"]:
+#     st.stop()
 
 penguin_file = st.file_uploader('Upload your own penguin data')
 # This first section imports the libraries that we need, adds the title – as we have
